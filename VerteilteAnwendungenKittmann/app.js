17a1fa5737d28635239dd405d5d1c0ae66f7989c
Philip <philip.kittmann@gmx.net>
2018-11-04 15:36:13 +0100

    completed exercise02
    bmi entries are now saved to a local database.

--- VerteilteAnwendungenKittmann/app.js
+++ VerteilteAnwendungenKittmann/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 // custom modules
 const bmiCalc = require(path.join(__dirname+'/public/modules/bmiCalc'));
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Lorwyn59",
+    database: "VerteilteAnwendungen"
+});
+
+conn.connect( (err) => {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
@@ -19,6 +32,11 @@ app.get('/bmi', (req, res) => {
 });
 app.post('/bmi', (req, res) => {
     let bmi = bmiCalc.calculateBMI(req.body.weight, req.body.height);
+    let sqlInsert = `INSERT INTO VerteilteAnwendungen.bmi_entries (name, weight, height, bmi, date) VALUES ('${req.body.name}', ${req.body.weight}, ${req.body.height}, ${bmi}, '${req.body.date}')`;
+    conn.query(sqlInsert, (err, result) => {
+        if(err) throw err;
+        console.log("Entry added successfully!");
+    });
     res.send("Your BMI is: " + bmi);
 });
 
